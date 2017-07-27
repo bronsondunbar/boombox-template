5a16369ba86801e8d8966127a2bb77299c82d5db
Bronson Dunbar <bronson@bronsondunbar.com>
2017-07-27 21:21:48 +0200

    Updates

--- source/js/app.js
+++ source/js/app.js
@@ -180,21 +180,21 @@ $(document).scroll(function(){
 
     if (screenResolution > 768) {
 
-      $('.showcase').each( function(i){
+      $(".showcase").each( function(i){
         
         var bottom_of_object = $(this).offset().top + 300;
         var bottom_of_window = $(window).scrollTop() + $(window).height();
         
         if (bottom_of_window > bottom_of_object) {
 
-            if (!$(this).hasClass('in-viewport')) {
-              $(this).velocity('transition.slideUpIn', { stagger: 700 }).delay(2000);
-              $(this).addClass('in-viewport');
+            if (!$(this).hasClass("in-viewport")) {
+              $(this).velocity("transition.slideUpIn", { stagger: 700 }).delay(2000);
+              $(this).addClass("in-viewport");
 
               if ($(this).hasClass("showcase-left")) {
-                $(this).find('.showcase-media').velocity({right: "-50%"}, { stagger: 700 }, { queue: false }, 5000);
+                $(this).find(".showcase-media").velocity({right: "-50%"}, { stagger: 700 }, { queue: false }, 5000);
               } else if ($(this).hasClass("showcase-right")) {
-                $(this).find('.showcase-media').velocity({left: "0%"}, { stagger: 700 }, { queue: false }, 5000);
+                $(this).find(".showcase-media").velocity({left: "0%"}, { stagger: 700 }, { queue: false }, 5000);
               }
             }
 
@@ -204,26 +204,26 @@ $(document).scroll(function(){
 
     }
 
-    $('#categories > svg > g').each( function(i){
+    $("#categories > svg > g").each( function(i){
         
         var bottom_of_object = $(this).offset().top + 300;
         var bottom_of_window = $(window).scrollTop() + $(window).height();
         
         if (bottom_of_window > bottom_of_object) {
 
-            if (!$(this).hasClass('wrapper')) {
-              $(this).addClass('wrapper');
+            if (!$(this).hasClass("wrapper")) {
+              $(this).addClass("wrapper");
               $(this).css("display", "block");
 
               /* SVG animation using AnimeJS */
 
-                var lineDrawing = anime({
-                  targets: '#categories .wrapper path',
+                var lineAnimation = anime({
+                  targets: "#categories .wrapper polygon",
                   strokeDashoffset: [anime.setDashoffset, 0],
-                  easing: 'easeInOutSine',
-                  duration: 6000,
+                  easing: "easeInOutSine",
+                  duration: 5000,
                   delay: function(el, i) { return i * 250 },
-                  direction: 'alternate',
+                  direction: "alternate",
                   loop: true
                 });
             }
